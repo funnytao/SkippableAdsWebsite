3461ada1e0909a7653ffc5464a836d076436efea
Tao Liu <taoliu404@gmail.com>
2020-09-22 00:03:27 -0400

    refine layout

--- src/layouts/VideoPageLayout.js
+++ src/layouts/VideoPageLayout.js
@@ -1,4 +1,5 @@
-import React, {useState} from 'react';
+import React from 'react';
+import queryString from 'query-string';
 import VideoUnit from '../components/VideoUnit';
 
 import MAIN_VIDEO from '../static/main_video.mp4';
@@ -6,18 +7,16 @@ import ADS_VIDEO from '../static/taotest.mp4';
 
 import classes from './VideoPageLayout.module.scss';
 
-console.log(classes)
-
 /**
  * Layout for the main page
  * @returns {component} main page layout
  */
 const VideoPageLayout = () => {
+    const { adTime = 0, skippable } = queryString.parse(window.location.search);
+
     return (
         <div className={classes.videoPageWrapper}>
-            <VideoUnit videoSource={MAIN_VIDEO} adsSource={ADS_VIDEO} adsTime={5} skippable />
-            <VideoUnit videoSource={MAIN_VIDEO} adsSource={ADS_VIDEO} adsTime={0} skippable />
-            <VideoUnit videoSource={MAIN_VIDEO} adsSource={ADS_VIDEO} adsTime={0} />
+            <VideoUnit videoSource={MAIN_VIDEO} adsSource={ADS_VIDEO} adsTime={Number(adTime)} skippable={Boolean(skippable)} />
         </div>
     );
 }
